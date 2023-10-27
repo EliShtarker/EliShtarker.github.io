bdb66c0142696fef1ae989385b33fffb5aba4692
RedTearStone <w4n7i75084d@gmail.com>
2023-10-27 13:10:23 +0300

    changed the site to what i was working on last night.

---
+++ js.js
@@ -0,0 +1,9 @@
+
+function change() {
+    document.getElementById("clickMe").innerHTML = "Click Me";
+}
+
+function transformation() {
+    document.getElementById("hyperLink").style.width = "90px";
+    document.getElementById("hyperLink").style.marginLeft = "44.8%"; 
+}
